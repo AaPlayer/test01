5b0642b757e1fdb9c1b22366c02bfb625dcd39a2
AaPlayer <461134195@qq.com>
2018-07-29 12:04:01 +0800

    Add files via upload
    first commit for django

---
+++ 111111/111111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  111111
+//
+//  Created by yan on 2018/7/29.
+//  Copyright © 2018年 yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 111111/111111/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  111111
+//
+//  Created by yan on 2018/7/29.
+//  Copyright © 2018年 yan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
